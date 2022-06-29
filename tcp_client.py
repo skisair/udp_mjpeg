cef96d5b96bbd9f55fb75ea8963d20bd15017fff
skisair <skis.air@mac.com>
2022-06-29 09:31:32 +0900

    #5 UDP Server/Client対応

--- tcp_client.py
+++ tcp_client.py
@@ -1,24 +1,10 @@
-import time
-
 import requests
-import cv2
-import numpy as np
 
 from jpeg_stream_viewer import JpegStreamViewer
 
-SOI = bytes.fromhex('0xFFD8'[2:])
-EOI = bytes.fromhex('0xFFD9'[2:])
-
-jpeg_stream_viewer = JpegStreamViewer()
 
 if __name__ == '__main__':
     r = requests.get('http://localhost:5000', stream=True)
-    buffer = b''
-    last_chunk_time = time.perf_counter()
-    last_frame_time = time.perf_counter()
-    chunk_times = []
-    frame_times = []
-    chunks = []
-    frames = []
+    jpeg_stream_viewer = JpegStreamViewer()
     for chunk in r.iter_content(chunk_size=1024):
         jpeg_stream_viewer.send(chunk)
--- udp_client.py
+++ udp_client.py
@@ -1,19 +1,22 @@
 import os
 import socket
 
-HOST_NAME = os.environ.get('UDP_TARGET', default='')
-PORT = int(os.environ.get('UDP_PORT', default='8080'))
+from jpeg_stream_viewer import JpegStreamViewer
 
+HOST_NAME = os.environ.get('UDP_TARGET', default='')
+PORT = int(os.environ.get('UDP_PORT', default='8000'))
 
 if __name__ == '__main__':
+
     # ipv4を使うので、AF_INET
     # udp通信を使いたいので、SOCK_DGRAM
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # ブロードキャストするときは255.255.255.255と指定せずに空文字
     sock.bind((HOST_NAME, PORT))
+    jpeg_stream_viewer = JpegStreamViewer()
     while True:
         # データを待ち受け
-        rcv_data, addr = sock.recvfrom(1024)
-        print("receive data : [{}]  from {}".format(rcv_data,addr))
+        rcv_data, addr = sock.recvfrom(65507)
+        jpeg_stream_viewer.send(rcv_data)
 
     sock.close()
